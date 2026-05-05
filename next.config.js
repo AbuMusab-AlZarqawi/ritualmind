bf49111165b9275d1e3fcfa8d27f00ada61a2301
AbuMusab-AlZarqawi <adilpedro59@email.com>
2026-05-05 07:32:00 +0100

    Fix webpack build errors

--- next.config.js
+++ next.config.js
@@ -1,2 +1,12 @@
 /** @type {import('next').NextConfig} */
-module.exports = { reactStrictMode: true };
+module.exports = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "@react-native-async-storage/async-storage": false,
+    };
+    return config;
+  },
+};
